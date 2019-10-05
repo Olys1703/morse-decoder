de20a4b157e4a0f408724de39a864e8026b2409c
SergeyLysenko <sergey.s.lysenko@sms-a.ru>
2019-10-05 17:49:07 +0400

    feat: add task

--- src/index.js
+++ src/index.js
@@ -39,6 +39,41 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let index
+    let jndex
+    let decoded_str = ''
+    let next_is_letter = false
+    let buffer = ''
+
+    for (index = 0; index < expr.length; index += 10) {
+        if (expr[index] == '*') {
+            decoded_str = decoded_str + ' '
+        }
+        else {
+            for ( jndex = index; jndex < (index + 10); jndex++ ) {
+                if (next_is_letter) {
+                    if (expr[jndex] == '1') {
+                        buffer = buffer + '-' 
+                    }
+                    else {
+                        buffer = buffer + '.' 
+                    }
+                    next_is_letter = false
+                }
+                else {
+                    if (expr[jndex] == '1') {
+                        next_is_letter = true
+                    }
+                }
+            }
+
+            decoded_str = decoded_str + MORSE_TABLE[buffer]
+            buffer = ''
+
+        }
+    }
+    
+    return decoded_str
 }
 
 module.exports = {
